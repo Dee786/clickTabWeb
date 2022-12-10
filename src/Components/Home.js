fba187fb0c7a5849911b5ea39d27aa9539a5184a
narottam-hypercare <narottam@hypercare.com>
2022-12-10 22:20:14 +0530

    run action

---
+++ src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Home() {
+    const Container = styled.div`
+    background-color:#F2F2F2;
+    width:100%;
+    min-height:676px;
+    `;
+  return (
+    <Container></Container>
+  )
+}
+
+export default Home
