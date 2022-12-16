5dce1be3beb8a500a7aa57c96ada1d67fe8218b5
narottam-hypercare <narottam@hypercare.com>
2022-12-16 21:55:25 +0530

    branch dashboard

--- src/utils/isLogin.js
+++ src/utils/isLogin.js
@@ -1,5 +1,5 @@
 import { encryptStorage } from "./encryptStorage";
 
 export const isLogin = () =>{
-    return encryptStorage.getItem("user") != undefined;
+    return encryptStorage.getItem("user_clicktabweb") != undefined;
 }
